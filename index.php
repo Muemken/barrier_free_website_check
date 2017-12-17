c40200a1d7f2809da30a2c41b5877f114e9017e5
alexander <alexander.muemken@gmx.de>
2017-12-17 01:27:56 +0100

    evaluation_list implemented

--- index.php
+++ index.php
@@ -29,6 +29,31 @@ if (NULL != filter_input(INPUT_GET, 'site', FILTER_SANITIZE_STRING)) {
     include_once "html/home.html";
 }
 
+
+$action = filter_input(INPUT_GET, 'action');
+switch ($action) {
+    case 'yes_list':
+        include "html/evaluation_list.html";
+        echo "yes_list";
+        break;
+    case 'no_list':
+        include "html/evaluation_list.html";
+        echo "no_list";
+        break;
+    case 'skip_list':
+        include "html/evaluation_list.html";
+        echo "skip_list";
+        break;
+    case 'notag_list':
+        include "html/evaluation_list.html";
+        echo "notag_list";
+        break;
+    case 'invalid_list':
+        include "html/evaluation_list.html";
+        echo "invalid_list";
+        break;
+}
+
 include_once "html/footer.html";
 echo '</body>';
 echo '</html>';
